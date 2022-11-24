e50ea879131e9015389e24ac77c4e26767365cde
Yeow Pann <83297826+sreyounpann@users.noreply.github.com>
2022-11-24 13:18:49 +0700

    Add files via upload
    First Time Upload the project

---
+++ Bus.cpp
@@ -0,0 +1,176 @@
+//
+//  Bus.cpp
+//  BusManagement_v3
+//
+//  Created by Yeow Pann on 14/11/22.
+//
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Bus{
+    
+private:
+    
+    int _busNumber;
+    string _driverName;
+    string _departureTime;
+    string _arrivalTime;
+    string _from;
+    string _to;
+    int count = 0;
+    
+public:
+    
+    Bus():_busNumber(0),_driverName("Unknow"),_departureTime("Unknow"), _arrivalTime("Unknow"),_from("Unknow"),_to("Unknow"){}
+    Bus (int _busNumber, string _driverName, string _departureTime, string _arrivalTime, string _from, string _to): _busNumber(_busNumber),_driverName(_driverName),_departureTime(_departureTime),_arrivalTime(_arrivalTime),_from(_from),_to(_to){}
+    ~Bus (){}
+    
+    void setBusNumber(int _busNumber){
+        if(_busNumber < 0){
+            
+            throw "\n\t\t\t\tINVALID INPUT\n";
+           
+            
+        }
+        else{
+            this->_busNumber = _busNumber;
+        }
+        
+    }
+    int getBusNumber(){
+        return _busNumber;
+    }
+    
+    void setDriverName(string _driverName){
+        this-> _driverName = _driverName ;
+        
+    }
+    
+    string getDriverName(){
+        return _driverName;
+    }
+    void setDepartureTime(string _departureTime){
+        this-> _departureTime = _departureTime;
+    }
+    string getDepartureTime(){
+        return _departureTime;
+    }
+    void setArrivalTime(string _arrivalTime){
+        this-> _arrivalTime = _arrivalTime;
+    }
+    string getArrivalTime(){
+        return _arrivalTime;
+    }
+    void setTo(string _to){
+        this-> _to = _to;
+    }
+    string getTo(){
+        return _to;
+    }
+    void setFrom(string _from){
+        this-> _from = _from;
+    }
+    string getFrom(){
+        return _from;
+    }
+    virtual void outputbeforeedit(){
+        
+        cout << "\n\n.......Diplay Result Before Modify......\n";
+        
+        for (int i=50;i>0;i--)
+
+        cout<<'-';
+
+        cout <<"\nBus ID : "<< _busNumber<<endl;
+        cout <<"\nDriver's Name : "<< _driverName<<endl;
+        cout <<"\nDeparture Time : "<< _departureTime<<endl;
+        cout <<"\nArrival Time : "<<_arrivalTime<<endl;;
+        cout <<"\nFrom Station : "<<_from<<endl;
+        cout <<"\nTo Station : "<<_to<<endl;
+        for (int i=50;i>0;i--)
+        cout<<'-';
+
+
+    }
+    virtual void outputbefordelete(){
+        
+        cout << "\n\n.......Diplay Result Before Delete......\n";
+        
+        for (int i=50;i>0;i--)
+
+        cout<<'-';
+
+        cout <<"\nBus ID : "<< _busNumber<<endl;
+        cout <<"\nDriver's Name : "<< _driverName<<endl;
+        cout <<"\nDeparture Time : "<< _departureTime<<endl;
+        cout <<"\nArrival Time : "<<_arrivalTime<<endl;;
+        cout <<"\nFrom Station : "<<_from<<endl;
+        cout <<"\nTo Station : "<<_to<<endl;
+        for (int i=50;i>0;i--)
+
+        cout<<'-';
+        cout << endl << endl;
+
+
+    }
+    
+    virtual void inputBus()
+    {
+        
+        cout << "\n\t\t--------------- FILL BUS INFORMATION ---------------\n";
+        
+        cout << "\nInput Bus Number: ";
+        
+        cin >> _busNumber;
+
+        cout << "\nInput Driver's Name: ";
+        
+        cin >> _driverName;
+        
+        cout << "\nInput Departure Time: ";
+        
+        cin >> _departureTime;
+        
+        cout << "\nInput Arrive Time: ";
+        
+        cin >> _arrivalTime;
+        
+        cout << "\nFrom: \t\t\t";
+        
+        cin >> _from;
+        
+        cout << "\nTo: \t\t\t";
+        
+        cin >> _to ;
+    }
+    
+    virtual void outputBus(){
+        
+        cout << "\n\n\t\t\t.......Bus Details......\n";
+        
+        cout<<"\nBus no: \t"<<_busNumber<<"\n";
+        for (int i=80;i>0;i--)
+
+        cout<<'-';
+
+        cout <<"\nDriver: \t"<<_driverName<<"\t\tArrival time: \t"
+
+        <<_arrivalTime<<"\tDeparture time: "<<_departureTime
+
+        <<"\nFrom: \t\t"<<_from<<"\t\tTo: \t\t"<<
+
+        _to<<"\n";
+        for (int i=80;i>0;i--)
+
+        cout<<'-';
+        
+        cout << endl << endl;
+       
+    }
+    
+    
+};
+
+
+
---
+++ UserManager.cpp
@@ -0,0 +1,151 @@
+//
+//  UserManager.cpp
+//  BusManagement_v3
+//
+//  Created by Yeow Pann on 15/11/22.
+//
+
+
+#pragma once
+#include <iostream>
+#include "User.cpp"
+using namespace std;
+
+class UserManager {
+private:
+    User* users[10];
+    int count = 0;
+    string username ;
+    string password ;
+    int loginAttempt = 0;
+    const int Count = 5;
+public:
+
+    void addUser(User* user) {
+        users [count] = new User();
+        users[count] = user;
+        count++;
+    }
+
+    void viewUser() {
+        cout << "\n\n\t\t\t\t.......Account Details......\n";
+        for (int i = 0; i < count;i++) {
+           
+            cout <<"\n\t\tUser ID: \t"<<users[i]->getId()<<"\t\tUsername: \t" <<users[i]->getUsername()<<"\t\tPassword: \t"<<users[i]->getPassword()<<"\t\t\n";
+
+        }
+    }
+
+    int findUersByName(string username) {
+        for (int i = 0; i < count; i++) {
+            if (users[i]->getUsername() == username) {
+                return i;
+            }
+            else {
+                          
+                          cout <<"\n\t\t\t\t>>> DATA NOT FOUND <<<"<< endl;
+                          
+                      }
+        }
+        return -1;
+    }
+    
+    int findUserById(int id){
+        for(int i = 0; i < count ; i++){
+            if (users[i]->getId()==id) {
+                return i;
+            }
+            else {
+                          
+                          cout <<"\n\t\t\t\t>>> DATA NOT FOUND <<<"<< endl;
+                          
+                      }
+            
+        }
+        return -1;
+        
+    }
+    void editUserbyId(int id){
+        
+        int found = findUserById(id);
+        users[found]->outputUser();
+        if(found != id){
+            cout << "\n\t\t\t\tINPUT NEW USER ID: ";
+            cin >> id;
+            users[found]->setId(id);
+            
+            cout << "\n\t\t\t\t >>> User ID has Been Edit <<< "<< endl;
+            
+        }
+        else {
+            cout << "\n\t\t\t\t >>> Data Not Found <<< " << endl;
+        }
+        
+        
+        
+    }
+    
+    void editUserbyName(string username) {
+ 
+
+        int found = findUersByName(username);
+        users[found]->outputUser();
+        if (found != -1) {
+           
+            cout << "\n\t\t\t\tINSERT  NEW USERNAME: ";
+            cin >> username;
+            users[found]->setUsername(username);
+
+        }
+        else {
+            cout << "\n\t\t\t\tNot found" << endl;
+        }
+    }
+    
+    void deleteUserbyName(string name) {
+
+        int found = findUersByName(name);
+        users[found]->outputUser();
+        if (found != -1) {
+            for (int i = found; i < count; i++) {
+                users[i] = users[i + 1];
+            }
+            count--;
+            cout << "\n\t\t\t\tSorry,this users has been delete\n";
+            
+        }
+        else {
+            cout << "\n\t\t\t\tNot found" << endl;
+        }
+    }
+    void deleteUserbyId(int id){
+        int found = findUserById(id);
+       
+        if (found != -1){
+            for(int i = found; i < count; i++){
+                users[i] = users[i+1];
+            }
+            count--;
+            cout << "\n\t\t\t\tSorry,this users has been delete\n";
+            
+        }
+        else {
+            cout << "\n\t\t\t\tNot found" << endl;
+        }
+        
+    }
+
+    User* authenticateUser(string username, string password) {
+        User* user=nullptr;
+        for (int i = 0; i < count; i++) {
+            if (users[i]->getUsername() == username && users[i]->getPassword() == password) {
+                return users[i];
+            }
+        }
+        return user;
+    }
+
+
+
+
+};
